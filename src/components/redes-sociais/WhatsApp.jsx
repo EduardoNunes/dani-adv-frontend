361b2d0b88265ece4eb002acf7ac47859dffcd69
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-02-05 17:16:23 -0300

    adicionado @babel/plugin-proposal-private-property-in-object em devDependences

--- src/components/redes-sociais/WhatsApp.jsx
+++ src/components/redes-sociais/WhatsApp.jsx
@@ -1,25 +1,27 @@
-import IconeWhatsAppBranco from '../../assets/whats-branco.png'
-import IconeWhatsAppPreto from '../../assets/whats-preto.png'
-import { useTheme } from '../../context/ThemeContext'
-import './styles.css'
+import IconeWhatsAppBranco from "../../assets/whats-branco.png";
+import IconeWhatsAppPreto from "../../assets/whats-preto.png";
+import { useTheme } from "../../context/ThemeContext";
+import "./styles.css";
 
 function WhatsApp({ numWhatsApp, icone }) {
-    const { theme } = useTheme()
+  const { theme } = useTheme();
 
-    if (!icone) {
-        icone = theme === 'light' ? IconeWhatsAppPreto : IconeWhatsAppBranco
-    }
+  if (!icone) {
+    icone = theme === "light" ? IconeWhatsAppPreto : IconeWhatsAppBranco;
+  }
 
-    return (
-        <div className='whats-app'>
-            <a href={`https://api.whatsapp.com/send?phone=${+5571993259187}&text=Olá, Dra. Daniela, preciso de um auxílio jurídico, vamos conversar?`}
-                target="_blank"
-                rel="noopener noreferrer">
-                <p>{numWhatsApp}</p>
-                <img src={icone} alt="Ícone Whats App" />
-            </a>
-        </div>
-    )
+  return (
+    <div className="whats-app">
+      <a
+        href={`https://api.whatsapp.com/send?phone=${+5571993259187}&text=Olá, Dra. Daniela, preciso de um auxílio jurídico, vamos conversar?`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p>{numWhatsApp}</p>
+        <img src={icone} alt="Ícone Whats App" />
+      </a>
+    </div>
+  );
 }
 
-export default WhatsApp
+export default WhatsApp;
